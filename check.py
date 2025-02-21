7b08c8d494ed9a0cc1d8b6211ab38e4cd8f3b1f7
mutantsan <kvaqich@gmail.com>
2025-02-21 14:58:23 +0200

    translation, part 7

---
+++ check.py
@@ -0,0 +1,15 @@
+import subprocess
+import shutil
+from pathlib import Path
+
+
+def main():
+    find_untranslated_lines()
+
+
+def find_untranslated_lines():
+    pass
+
+
+if __name__ == "__main__":
+    main()
